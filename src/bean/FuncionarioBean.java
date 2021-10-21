69ef29a76a97aca9f8ff553da3894bee716e7ae8
vitornespolo <vitornespolo@gmail.com>
2021-10-21 14:32:12 -0300

    updateSQL

--- src/bean/FuncionarioBean.java
+++ src/bean/FuncionarioBean.java
@@ -21,13 +21,13 @@ public class FuncionarioBean extends AbstractBeanImpl<Funcionario>{
 
 	@Override
 	public List<Funcionario> buscarTodos() throws Exception {
-		String sql = "Select a From Autor a ";
+		String sql = "Select a From Funcionario f ";
 		Query query = entity.createQuery(sql);
 		return query.getResultList();
 	}
 	
 	public List<Funcionario> pesquisar(String nome)throws Exception {
-		String sql = "Select c From Autor c where upper(c.nome) like :nome ";
+		String sql = "Select c From Funcionario c where upper(c.nome) like :nome ";
 		Query query = entity.createQuery(sql);
 		query.setParameter("nome", "%"+nome.trim().toUpperCase()+"%");
 		return query.getResultList();
